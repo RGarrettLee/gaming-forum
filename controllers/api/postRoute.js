864fce0bb40d0fa53853db60d4528e77f6c3a67b
Zelda <lumakawar@hotmail.com>
2022-06-29 13:44:45 -0400

    minorchanges

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
         include: [User],
       });
       const posts = postData.map((post) => post.get({ plain: true }));
-      res.render('all-posts', { posts });
+      res.render('board', { posts });
     } catch (err) {
       res.status(500).json(err);
     }
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
       });
       if (postData) {
         const post = postData.get({ plain: true });
-        res.render('single-post', { post });
+        res.render('post', { post });
       } else {
         res.status(404).end();
       }
