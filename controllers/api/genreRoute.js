a2cc859d13da9ae57d026148d3c97e6c0d0acda6
Zelda <lumakawar@hotmail.com>
2022-06-30 12:00:38 -0400

    scriptsFirstDraft

--- controllers/api/genreRoute.js
+++ controllers/api/genreRoute.js
@@ -2,3 +2,7 @@ const express = require("express");
 const router = express.Router();
 const withAuth = require("../../utils/auth");
 const { Post, User } = require("../../models");
+
+
+
+module.exports = router;
