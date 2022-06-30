9ba673d1a964cff051aba966e76357b5f20ee4be
russelldnn <russelldnn@gmail.com>
2022-06-30 11:02:46 -0500

    css stuff

--- controllers/api/boardRoute.js
+++ controllers/api/boardRoute.js
@@ -2,3 +2,7 @@ const express = require("express");
 const router = express.Router();
 const withAuth = require("../../utils/auth");
 const { Board, User } = require("../../models");
+
+
+//me
+module.exports = router;
--- controllers/api/genreRoute.js
+++ controllers/api/genreRoute.js
@@ -2,3 +2,6 @@ const express = require("express");
 const router = express.Router();
 const withAuth = require("../../utils/auth");
 const { Post, User } = require("../../models");
+
+// me
+module.exports = router;
