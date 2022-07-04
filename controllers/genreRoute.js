93a7e762225573a81d5ff8dfc027f2f2015c3ad4
RGarrettLee <reginaldgarrettlee@gmail.com>
2022-07-04 13:52:21 -0400

    added variable for logout button

--- controllers/genreRoute.js
+++ controllers/genreRoute.js
@@ -6,13 +6,13 @@ router.get('/', async (req, res) => {
     const postData = await Post.findAll();
     const posts = postData.map((post) => post.get({ plain: true }));
     const filtered = posts.filter((v, i, a) => a.findIndex(v2 => ( v2.genre === v.genre)) === i);
-    res.status(200).render('postByGenre', { 'posts': filtered });
+    res.status(200).render('postByGenre', { 'posts': filtered, loggedIn: req.session.logged_in });
 });
 
 router.get('/:genre', async (req, res) => {
     const postData = await Post.findAll({ where: { genre: req.params.genre },include:[User]});
     const posts = postData.map((post) => post.get({ plain: true }));
-    res.status(200).render('genre', { posts }); // replace handlebar with appropriate handlebar
+    res.status(200).render('genre', { posts, loggedIn: req.session.logged_in }); // replace handlebar with appropriate handlebar
 });
 
 module.exports = router;
